754fdc0bbb7b4bf61555a433b3d84d86dfa4412d
user <slbn.public@gmail.com>
2025-01-30 18:30:44 +0200

    HW 10.2: Implement FileLogger with INFO/DEBUG levels and file size restriction

---
+++ src/main/java/org/example/Lesson10_2/FileLoggerConfigurationLoader.java
@@ -0,0 +1,23 @@
+package org.example.Lesson10_2;
+
+import java.io.*;
+import java.util.*;
+
+public class FileLoggerConfigurationLoader {
+
+    public static FileLoggerConfiguration load(String configFilePath) throws IOException{
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream(configFilePath)) {
+            properties.load(input);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String filePath = properties.getProperty("FILE");
+        LoggingLevel level = LoggingLevel.valueOf(properties.getProperty("LEVEL"));
+        long maxSize = Long.parseLong(properties.getProperty("MAX-SIZE"));
+        String format = properties.getProperty("FORMAT");
+
+        return new FileLoggerConfiguration(filePath, level, maxSize, format);
+    }
+}
---
+++ src/main/java/org/example/Lesson10_2/FileMaxSizeReachedException.java
@@ -0,0 +1,7 @@
+package org.example.Lesson10_2;
+
+public class FileMaxSizeReachedException extends Exception {
+    public FileMaxSizeReachedException(String message) {
+        super(message);
+    }
+}
